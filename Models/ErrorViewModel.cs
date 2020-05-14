751df44265f5a84c6537834ef686c7789bb8c521
Mohcka <mikeduels@yahoo.com>
2020-05-14 12:59:14 -0700

    Init Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace michaelWalker_pipelineDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
